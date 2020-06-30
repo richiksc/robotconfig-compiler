7f3ed9863c1f0c9c92c08f144417e18b2bf50a58
Richik SC <sc.richik@gmail.com>
2020-06-30 10:41:57 -0500

    More metadata updates

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -2,19 +2,18 @@ object PluginCoordinates {
     const val ID = "codes.richik.robotconfigcompiler.plugin"
     const val GROUP = "codes.richik.robotconfigcompiler"
     const val VERSION = "1.0.0"
-    const val IMPLEMENTATION_CLASS = "codes.richiksc.gradle.robotconfigcompiler.RobotConfigPlugin"
+    const val IMPLEMENTATION_CLASS = "codes.richiksc.robotconfigcompiler.RobotConfigPlugin"
 }
 
 object PluginBundle {
     const val VCS = "https://github.com/richiksc/robotconfig-compiler"
     const val WEBSITE = "https://github.com/richiksc/robotconfig-compiler"
-    const val DESCRIPTION = "An empty Gradle plugin created from a template"
-    const val DISPLAY_NAME = "An empty Gradle Plugin from a template"
+    const val DESCRIPTION = "Experimental static Java generator for Team 1816's RobotConfig format"
+    const val DISPLAY_NAME = "RobotConfig Compiler"
     val TAGS = listOf(
         "plugin",
         "gradle",
-        "sample",
-        "template"
+        "frc"
     )
 }
 
